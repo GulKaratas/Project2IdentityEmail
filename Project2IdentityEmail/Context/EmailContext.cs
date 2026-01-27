2c99650beeba4f8ac0a64eaf310c8aebe3c65ea3
GulKaratas <glsrn_7872@outlook.com>
2026-01-27 15:56:25 +0300

    Add project files.

---
+++ Project2IdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project2IdentityEmail.Entities;
+
+namespace Project2IdentityEmail.Context
+{
+    public class EmailContext : IdentityDbContext<AppUser>
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Initial Catalog=Project2EmailDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Project2IdentityEmail/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project2IdentityEmail.Dtos;
+using Project2IdentityEmail.Entities;
+
+namespace Project2IdentityEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }   
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserRegisterDto createUserRegisterDto)
+        {
+            AppUser appUser = new AppUser
+            {
+                Name = createUserRegisterDto.Name,
+                Surname = createUserRegisterDto.Surname,
+                UserName = createUserRegisterDto.Username,
+                Email = createUserRegisterDto.Email
+            };
+            await _userManager.CreateAsync(appUser, createUserRegisterDto.Password);
+            return RedirectToAction("UserList");
+        }
+    }
+}
